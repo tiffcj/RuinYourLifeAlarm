b5ede049369d60ab0c01624645ad0ad218d97ae8
tiffcj <tiffanyjiang598@gmail.com>
2017-12-02 16:24:17 -0600

    Remove unused imports in MainActivity

--- app/src/main/java/com/ruinyourlifealarm/ruinyourlifealarm/MainActivity.java
+++ app/src/main/java/com/ruinyourlifealarm/ruinyourlifealarm/MainActivity.java
@@ -5,16 +5,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.ruinyourlifealarm.ruinyourlifealarm.business.AccessMessages;
 import com.ruinyourlifealarm.ruinyourlifealarm.persistence.DatabaseHandler;
-import com.ruinyourlifealarm.ruinyourlifealarm.persistence.Message;
-
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-
 
         DatabaseHandler db = new DatabaseHandler(this);
         Main.setDatabase(db);
