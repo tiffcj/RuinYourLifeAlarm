af0895bc5dde5d3d14cea0a75c719800196aa06a
jbondoc <bondocj@myumanitoba.ca>
2017-12-02 14:56:59 -0600

    Add texting and alarm

---
+++ app/src/main/java/com/ruinyourlifealarm/ruinyourlifealarm/business/AlarmReceiver.java
@@ -0,0 +1,13 @@
+package com.ruinyourlifealarm.ruinyourlifealarm.business;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmReceiver extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_receiver);
+    }
+}
